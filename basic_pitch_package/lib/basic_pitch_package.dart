a19259dcc01e63c477736a1eb658495c3c537e81
Molly Sophia <mollysophia379@gmail.com>
2024-11-05 20:32:10 +0800

    wav loading and audio preprocess
    Signed-off-by: Molly Sophia <mollysophia379@gmail.com>

--- basic_pitch_package/lib/basic_pitch_package.dart
+++ basic_pitch_package/lib/basic_pitch_package.dart
@@ -1,6 +1,7 @@
 library basic_pitch_package;
-import 'dart:ffi';
 import 'dart:typed_data';
+import 'dart:io';
+import 'dart:convert';
 import 'package:flutter/services.dart';
 import 'package:onnxruntime/onnxruntime.dart';
 
@@ -16,7 +17,7 @@ class BasicPitch {
     _session = OrtSession.fromBuffer(bytes, sessionOptions);
   }
 
-  Future<List<List<List<List<double>>>>> predict(Float32List data, bool asyncRun) async {
+  Future<List<List<List<List<double>>>>> inference(Float32List data, bool asyncRun) async {
     if (data.length != 43844) {
       throw ArgumentError('Input data length must be 43844');
     }
@@ -42,5 +43,111 @@ class BasicPitch {
 
   void release() {
     _session.release();
+  }
+
+  Future<Map<String, Object>> loadAudioMono(String filePath) async {
+    final file = File(filePath);
+    final bytes = await file.readAsBytes();
+    final byteData = ByteData.sublistView(bytes);
+
+    if (utf8.decode(bytes.sublist(0, 4)) != 'RIFF' || utf8.decode(bytes.sublist(8, 12)) != 'WAVE') {
+      throw const FormatException('Unsupported file format');
+    }
+
+    int offset = 12;
+    ByteData fmt = ByteData(0);
+    while (offset < bytes.length) {
+      final chunkHeader = bytes.sublist(offset, offset + 8);
+      final chunkSize = byteData.getUint32(offset + 4, Endian.little);
+      offset += 8;
+
+      if (utf8.decode(chunkHeader.sublist(0, 4)) == 'fmt ') {
+        fmt = ByteData.sublistView(bytes, offset, offset + chunkSize);
+        offset += chunkSize;
+        break;
+      } else if (utf8.decode(chunkHeader.sublist(0, 4)) == 'JUNK') {
+        offset += chunkSize;
+      } else {
+        throw FormatException('Unsupported chunk type: ${utf8.decode(chunkHeader.sublist(0, 4))}');
+      }
+    }
+
+    final audioFormat = fmt.getUint16(0, Endian.little);
+    final numChannels = fmt.getUint16(2, Endian.little);
+    final sampleRate = fmt.getUint32(4, Endian.little);
+    final byteRate = fmt.getUint32(8, Endian.little);
+    final blockAlign = fmt.getUint16(12, Endian.little);
+    final bitsPerSample = fmt.getUint16(14, Endian.little);
+
+    print('audio_format: $audioFormat');
+    print('num_channels: $numChannels');
+    print('sample_rate: $sampleRate');
+    print('byte_rate: $byteRate');
+    print('block_align: $blockAlign');
+    print('bits_per_sample: $bitsPerSample');
+
+    final dataHeader = bytes.sublist(offset, offset + 8);
+    if (utf8.decode(dataHeader.sublist(0, 4)) != 'data') {
+      throw const FormatException('Unsupported data header');
+    }
+    final dataSize = byteData.getUint32(offset + 4, Endian.little);
+    offset += 8;
+
+    final rawData = bytes.sublist(offset, offset + dataSize);
+    List<double> data;
+    if (bitsPerSample == 16) {
+      final int16Data = Int16List.sublistView(Uint8List.fromList(rawData));
+      data = int16Data.map((e) => e / 32768.0).toList();
+    } else if (bitsPerSample == 32) {
+      final int32Data = Int32List.sublistView(Uint8List.fromList(rawData));
+      data = int32Data.map((e) => e / 2147483648.0).toList();
+    } else {
+      throw FormatException('Unsupported bit depth: $bitsPerSample');
+    }
+
+    if (numChannels == 2) {
+      final List<double> monoData = [];
+      for (int i = 0; i < data.length; i += 2) {
+        monoData.add((data[i] + data[i + 1]) / 2);
+      }
+      data = monoData;
+    }
+
+    return {'data': data, 'sampleRate': sampleRate};
+  }
+
+  Future<List<List<double>>> loadAudioWithPreProcess(String filePath) async {
+    final audio_out = await loadAudioMono(filePath);
+    final data = audio_out['data'] as List<double>;
+    final sampleRate = audio_out['sampleRate'] as int;
+    if (sampleRate != 22050) {
+      // TODO: add resample support
+      throw ArgumentError('Unsupported sample rate');
+    }
+
+    const windowSize = 43844;
+    const overlapLength = 7680;
+    final hopLength = windowSize - overlapLength;
+    final paddedData = List<double>.filled(overlapLength ~/ 2, 0.0) + data;
+
+    final totalSamples = paddedData.length;
+    final numWindows = (totalSamples / hopLength).ceil();
+    final windows = <List<double>>[];
+
+    for (int i = 0; i < numWindows; i++) {
+      final start = i * hopLength;
+      final end = start + windowSize;
+
+      List<double> window;
+      if (end > totalSamples) {
+        window = paddedData.sublist(start) + List<double>.filled(end - totalSamples, 0.0);
+      } else {
+        window = paddedData.sublist(start, end);
+      }
+
+      windows.add(window);
+    }
+
+    return windows;
   }
 }
--- basic_pitch_package/test/basic_pitch_package_test.dart
+++ basic_pitch_package/test/basic_pitch_package_test.dart
@@ -4,19 +4,32 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:basic_pitch_package/basic_pitch_package.dart';
 import 'dart:typed_data';
+import 'dart:io';
+import 'dart:core';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
   test('Test', () async {
     final basicPitchInstance = BasicPitch();
     basicPitchInstance.init();
-    Float32List data = Float32List(43844);
-    final result = await basicPitchInstance.predict(data, false);
-    for (int i = 0; i < 3; i++) {
-      for (int j = 0; j < 10; j++) {
-        print("${result[i][0][0][j]}");
+
+    final windowedAudioData = await basicPitchInstance.loadAudioWithPreProcess('assets/testdata/test_audio.wav');
+    for (int i = 0; i < windowedAudioData.length; i++) {
+      if (windowedAudioData[i].length != 43844) {
+        throw ArgumentError('Windowed audio data length must be 43844');
       }
-      print("==============");
+
+      final expected = File('assets/testdata/test_input_$i.bin').readAsBytesSync();
+      final expectedData = Float32List.sublistView(Uint8List.fromList(expected));
+      final data = Float32List.fromList(windowedAudioData[i]);
+      for (int j = 0; j < 43844; j++) {
+        if ((expectedData[j] - data[j]).abs() > 1e-5) {
+          throw ArgumentError('Data mismatch at index $j');
+        }
+      }
+      print("Windowed audio data $i matches expected data");
+      final inferenceResult = await basicPitchInstance.inference(data, false);
+      // test inference result here
     }
     basicPitchInstance.release();
   });
