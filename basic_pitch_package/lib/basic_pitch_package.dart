15b413e40fb8fe75a4da89a50b7ecb5ec06a2e97
Molly Sophia <mollysophia379@gmail.com>
2024-11-05 14:35:20 +0800

    playing around
    Signed-off-by: Molly Sophia <mollysophia379@gmail.com>

--- basic_pitch_package/lib/basic_pitch_package.dart
+++ basic_pitch_package/lib/basic_pitch_package.dart
@@ -1,14 +1,39 @@
 library basic_pitch_package;
-import 'package:onnxruntime/onnxruntime.dart';
+import 'dart:typed_data';
 import 'package:flutter/services.dart';
+import 'package:onnxruntime/onnxruntime.dart';
 
 class Basic_Pitch {
+  late OrtSession _session;
+
   void init() async {
     OrtEnv.instance.init();
-    // final sessionOptions = OrtSessionOptions();
-    // const modelPath = 'assets/models/nmp.onnx';
-    // final rawAssetFile = await rootBundle.load(modelPath);
-    // final bytes = rawAssetFile.buffer.asUint8List();
-    // final session = OrtSession.fromBuffer(bytes, sessionOptions!);
+    final sessionOptions = OrtSessionOptions();
+    const modelPath = 'assets/models/nmp.onnx';
+    final rawAssetFile = await rootBundle.load(modelPath);
+    final bytes = rawAssetFile.buffer.asUint8List();
+    _session = OrtSession.fromBuffer(bytes, sessionOptions);
   }
+
+  Future<bool> predict(Float32List data, bool asyncRun) async {
+    if (data.length != 43844) {
+      throw ArgumentError('Input data length must be 43844');
+    }
+    final shape = [1, 43844, 1];
+    final inputOrt = OrtValueTensor.createTensorWithDataList(data, shape);
+    final inputs = {'serving_default_input_2:0': inputOrt};
+    final runOptions = OrtRunOptions();
+    final List<OrtValue?>? outputs;
+    if (asyncRun) {
+      outputs = await _session.runAsync(runOptions, inputs);
+    } else {
+      outputs = _session.run(runOptions, inputs);
+    }
+    // TODO
+    inputOrt.release();
+    runOptions.release();
+    return true;
+  }
+
+  
 }
--- basic_pitch_package/test/basic_pitch_package_test.dart
+++ basic_pitch_package/test/basic_pitch_package_test.dart
@@ -1,10 +1,16 @@
+import 'dart:ffi';
+
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:basic_pitch_package/basic_pitch_package.dart';
+import 'dart:typed_data';
 
 void main() {
-  test('adds one to input values', () {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  test('Test', () {
     final basic_pitch = Basic_Pitch();
     basic_pitch.init();
+    Float32List data = Float32List(43844);
+    basic_pitch.predict(data, false);
   });
 }
