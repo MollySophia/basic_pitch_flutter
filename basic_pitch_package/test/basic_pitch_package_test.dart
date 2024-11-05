d242c6357b4b7e08c39b0a89f2460e0de920240f
Molly Sophia <mollysophia379@gmail.com>
2024-11-05 16:27:18 +0800

    fixup! return lists of tensors in predict function

--- basic_pitch_package/test/basic_pitch_package_test.dart
+++ basic_pitch_package/test/basic_pitch_package_test.dart
@@ -7,11 +7,17 @@ import 'dart:typed_data';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
-  test('Test', () {
+  test('Test', () async {
     final basicPitchInstance = BasicPitch();
     basicPitchInstance.init();
     Float32List data = Float32List(43844);
-    final result = basicPitchInstance.predict(data, false);
+    final result = await basicPitchInstance.predict(data, false);
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 10; j++) {
+        print("${result[i][0][0][j]}");
+      }
+      print("==============");
+    }
     basicPitchInstance.release();
   });
 }
